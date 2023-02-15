4232cbae8c798991c58d48268f6a51673c145eb5
mikus151 <mikus151@wp.pl>
2023-02-15 22:32:25 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const allIngredients = document.querySelector("#ingredients");
+ingredients.forEach((ingredient) => {
+  const newElement = document.createElement("li");
+  newElement.textContent = ingredient;
+  newElement.classList.add("item");
+  allIngredients.append(newElement);
+});
