665490b979c49653fdad9b08e131872eeb685a4d
mikus151 <mikus151@wp.pl>
2023-02-17 22:07:17 +0100

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+
+
+const inputName = document.querySelector("#name-input");
+const outputName = document.querySelector("#name-output");
+
+inputName.addEventListener("input", (event) => {
+  if (event.currentTarget.value) {
+    outputName.textContent = event.currentTarget.value;
+  } else {
+    outputName.textContent = 'Anonymous'
+  }
+});
