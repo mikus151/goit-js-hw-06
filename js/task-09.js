5062cc27d823321be9768f32a9d1cc7bdb370285
mikus151 <mikus151@wp.pl>
2023-02-19 19:54:16 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bgColor = document.querySelector("body");
+const btnChangeColor = document.querySelector(".change-color");
+const nameBgColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  bgColor.style.backgroundColor = randomColor;
+  nameBgColor.textContent = randomColor;
+});
