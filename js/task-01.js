c160317441202a4d5fd0f7f0bd7355630050093b
mikus151 <mikus151@wp.pl>
2023-02-20 08:29:36 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const allCategories = document.querySelector("#categories");
 const categories = allCategories.querySelectorAll(".item");
-console.log(`Number of categories: ${allCategories.length}`);
+console.log(`Number of categories: ${categories.length}`);
 categories.forEach((item) => {
   console.log(`Category: ${item.firstElementChild.textContent}`);
   console.log(`Elements: ${item.lastElementChild.children.length}`);
